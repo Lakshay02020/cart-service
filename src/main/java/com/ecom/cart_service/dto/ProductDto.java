08b1401975bfb0105751de3d28ffb2b77a440bd2
Lakshay Singla <lakshay02singla@gmail.com>
2025-05-05 11:41:59 +0530

    Added product feign to fetch prices

---
+++ src/main/java/com/ecom/cart_service/dto/ProductDto.java
@@ -0,0 +1,17 @@
+package com.ecom.cart_service.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ProductDto {
+    private Long id;
+    private Double price;
+}
